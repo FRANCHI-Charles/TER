fb888dd6026c977f886c21e23a18ad7d18378514
FRANCHI-Charles <charles.franchi@yahoo.fr>
2022-12-09 16:12:31 +0100

    PresDec3

--- src/PresentationDec.lean
+++ src/PresentationDec.lean
@@ -3,7 +3,6 @@ import tactic
 
 namespace vilnius
 
-/- Quelques bases -/
 
 example (P Q : Prop) : P ∧ Q → Q :=
 begin
@@ -20,43 +19,5 @@ begin
   exact hP,
   exact hQ,
 end
-
-
-example (P Q : Prop) : (P → ¬ Q) → (Q → ¬ P) :=
-begin
-  intro hPnQ,
-  intro hQ,
-  intro hP,
-  apply hPnQ,
-  exact hP,
-  exact hQ,
-end
-
-
-/- 
-
-Exemple avec les limites
-
--/
-
-definition is_limit (a : ℕ → ℝ) (l : ℝ) : Prop :=
-∀ ε > 0, ∃ N, ∀ n ≥ N, | a n - l | < ε
-
-
-theorem is_limit_add {a : ℕ → ℝ} {l : ℝ} (c : ℝ) (ha : is_limit a l) :
-  is_limit (λ i, a i + c) (l + c) :=
-begin
-  intros e se,
-  specialize ha e se,
-  cases ha with N ha,
-  use N,
-  intros n sn,
-  simp,
-  specialize ha n sn,
-  exact ha,
-end
-
-
-
 
 end vilnius
