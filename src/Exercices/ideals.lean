d02525e4fd9b04257798913d569409614c333d10
FRANCHI-Charles <charles.franchi@yahoo.fr>
2023-01-28 10:27:43 +0100

    2801

--- src/Exercices/ideals.lean
+++ src/Exercices/ideals.lean
@@ -11,19 +11,45 @@ de Atiyah-Macdonald -/
 example (a b : R) : a ∈ I → b ∈ I → (a + b) ∈ I :=
 begin
   intros ha hb,
-
+exact I.add_mem ha hb, /- différence entre I.add_mem et add_mem ? -/
 end
 
-example (a x : R) : a ∈ I → (a * x) ∈ I ∧ (x * a) ∈ I := sorry
+
+example (a x : R) : a ∈ I → (a * x) ∈ I ∧ (x * a) ∈ I :=
+begin
+  intro ha,
+  split,
+  rewrite mul_comm,
+  all_goals {
+    apply I.smul_mem,
+    assumption},
+end
 
 
 /- Est-ce que vous pouvez expliquer pourquoi l'énoncé "la préimage d'un idéal par un morphisme
 d'anneaux" est une `définition` et pas un `lemme`? -/
 definition preimage (f : R →+* S) (J : ideal S) : (ideal R) :=
 { carrier := {x : R | f x ∈ J},
-  add_mem' := sorry,
-  zero_mem' := sorry,
-  smul_mem' := sorry }
+  add_mem' :=
+  begin
+    simp,
+    intros a b hfa hfb,
+    apply J.add_mem hfa hfb,
+  end,
+
+  zero_mem' :=
+  begin
+    simp only [set.mem_set_of_eq, map_zero, submodule.zero_mem],
+  end,
+
+  smul_mem' :=
+  begin
+    intros x r hfr,
+    simp only [smul_eq_mul, set.mem_set_of_eq, map_mul],
+    apply J.smul_mem,
+    sorry,
+  end
+   }
 
 example (f : R →+* S) (J : ideal S) (hJ: J.is_prime) : (preimage f J).is_prime := sorry
 
