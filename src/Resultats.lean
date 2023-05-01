23707feea49967554be2529f29c31bd228859f88
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-05-01 12:03:57 +0200

    added def of extended

--- src/Resultats.lean
+++ src/Resultats.lean
@@ -1,3 +1,4 @@
+import algebra.module.localized_module
 import ring_theory.localization.basic
 import ring_theory.localization.module
 import algebra.module.linear_map
@@ -5,9 +6,14 @@ import algebra.module.linear_map
 open is_localization
 
 
-variables {R : Type*} (S : Type*) [comm_semiring R] [comm_semiring S] [algebra R S]
+variables {R : Type*} [comm_semiring R] (S : submonoid R)
 variables {M : Type*} [add_comm_monoid M] [module R M]
 variables {N : Type*} [add_comm_monoid N] [module R N]
+-- variables (hM : localized_module S M) (hN : localized_module S N)
 
 
-def extended {f : M →+ N} [h : is_linear_map R f] :
+def extended (f : M →[R] N) : (localized_module S M) →+[localization S] (localized_module S N) :=
+{ to_fun := sorry,
+  map_smul' := sorry,
+  map_zero' := sorry,
+  map_add' := sorry }
