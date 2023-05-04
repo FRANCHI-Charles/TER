f15ef05ad58a5d1f1a88fe13ba1739dfe7adf73a
FRANCHI-Charles <charles.franchi@yahoo.fr>
2023-05-05 01:28:49 +0200

    0505

--- src/Resultats.lean
+++ src/Resultats.lean
@@ -12,13 +12,28 @@ variables {N : Type*} [add_comm_monoid N] [module R N]
 -- variables (hM : localized_module S M) (hN : localized_module S N)
 
 
-def sec (b : localized_module S M) : M × S :=
+lemma mk_wd (f : M →[R] N) : ∀ (p p' : M × S) (h1 : p ≈ p'), mk (f p.1) p.2 = mk (f p'.1) (p'.2):=
 begin
-  
+  intros x y hr,
+  rw mk_eq,
+  have h : ∃ (u : ↥S), u • x.snd • y.fst = u • y.snd • x.fst,
+
+  swap,
+  cases h with u hu,
+  use u,
+  have premier := map_smul f,
+  have deuxieme := premier x.2 y.1, --fonctionne, mais je ne peux le faire en une ligne...
+  rw ← deuxieme, -- x.2 ≠ ↑x.2  
 end
+/-
+1 Trouver la bonne fonction pour le porblème de rw deuxième
+2 Appliquer congr_arg et résoudre
+3 comprendre ≃ et résoudre sans le swap
+4 compléter extended -/  
+
 
 def extended (f : M →[R] N) : (localized_module S M) →+[localization S] (localized_module S N) :=
-{ to_fun := λ p1, lift_on p1 (λ x, mk (f(x.1)) (x.2)),
+{ to_fun := λ p1, lift_on p1 (λ x, mk (f(x.1)) (x.2)) (mk_wd S f),
   map_smul' := sorry,
   map_zero' := sorry,
   map_add' := sorry }
