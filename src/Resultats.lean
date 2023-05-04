6b38f82d72f9691060cc63074c9f688f42f73b92
FRANCHI-Charles <charles.franchi@yahoo.fr>
2023-05-04 19:26:34 +0200

    0405

--- src/Resultats.lean
+++ src/Resultats.lean
@@ -3,7 +3,7 @@ import ring_theory.localization.basic
 import ring_theory.localization.module
 import algebra.module.linear_map
 
-open is_localization
+open localized_module
 
 
 variables {R : Type*} [comm_semiring R] (S : submonoid R)
@@ -12,8 +12,13 @@ variables {N : Type*} [add_comm_monoid N] [module R N]
 -- variables (hM : localized_module S M) (hN : localized_module S N)
 
 
+def sec (b : localized_module S M) : M × S :=
+begin
+  
+end
+
 def extended (f : M →[R] N) : (localized_module S M) →+[localization S] (localized_module S N) :=
-{ to_fun := sorry,
+{ to_fun := λ p1, lift_on p1 (λ x, mk (f(x.1)) (x.2)),
   map_smul' := sorry,
   map_zero' := sorry,
   map_add' := sorry }
